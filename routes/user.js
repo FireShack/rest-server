9aa02739be437b54f12abda3ab44a31801d9f9a6
Franco <francoguardini10@gmail.com>
2022-04-23 19:04:01 -0300

    POST validation updated

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { check } = require("express-validator");
 
 // Routes controllers
 const {
@@ -9,10 +10,24 @@ const {
   handleDelete,
   handleDefault,
 } = require("../controller/users.controler.js");
+const { validateFields } = require("../middlewares/validate.fields.js");
 
 // Routes
 router.get("/", handleGetAllUsers);
-router.post("/", handlePost);
+router.post(
+  "/",
+  [
+    check("name", "Name value is empty").not().isEmpty(),
+    check("pass", "Password must have more than 6 digits").isLength({ min: 6 }),
+    check("mail", "Your mail is not valid").isEmail(),
+    check("role", "Role selected does not exists").isIn([
+      "ADMIN_ROLE",
+      "USER_ROLE",
+    ]),
+    validateFields,
+  ],
+  handlePost
+);
 router.patch("/:id", handlePatch);
 router.delete("/", handleDelete);
 router.get("*", handleDefault);
