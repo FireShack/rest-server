e26ae9512f9847041e17c71688e07b9cf2c1787f
Franco <francoguardini10@gmail.com>
2022-04-23 19:28:48 -0300

    new comments added

--- middlewares/validate.fields.js
+++ middlewares/validate.fields.js
@@ -7,6 +7,7 @@ const validateFields = (req, res, next) => {
   if (!errors.isEmpty()) {
     return res.status(400).json(errors);
   }
+  // Goes to the next validation or to the controller
   next();
 };
 
