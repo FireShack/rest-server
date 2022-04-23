2b7172ff611b515be8648b546785cac3c0211dda
Franco <francoguardini10@gmail.com>
2022-04-23 19:05:22 -0300

    middlewares, packages and validations updated

--- controller/users.controler.js
+++ controller/users.controler.js
@@ -1,19 +1,34 @@
+const bcrypt = require("bcryptjs");
+const User = require("../models/user.model");
+const fs = require("fs");
+
 // Users container
 let users = [];
-const User = require("../models/user.model")
 
 const handleGetAllUsers = (req, res) => res.json({ status: 200, msg: users });
 
-const handlePost = (req, res) => {
-  const { username, age } = req.body;
+const handlePost = async (req, res) => {
+  // Post request data, this will be saved into the DB
+  const { name, mail, pass, role } = req.body;
+
+  // We declare the new instance, and then we send all the request data
+  const user = new User({ name, mail, pass, role });
+
   try {
-    if (!username || !age) {
-      res.status(400).send("Please, complete the required data");
-    }
-    users.push({username, age });
-    res.json({ status: 200, msg: `User added successfully` });
+    // Password Encryptation
+    const salt = bcrypt.genSaltSync();
+    user.pass = bcrypt.hashSync(pass, salt);
+
+    // Save user into the DB
+    users.push({ user });
+    await user.save();
+
+    // Server response
+    res.status(200).json({ msg: `User added successfully` });
   } catch (err) {
-    res.json({ status: 400, msg: `The arguments are incorrect: ${err}` });
+    res.status(400).json({ msg: `The arguments are incorrect: ${err}` });
+    // This function writes a log file to save all the errors
+    fs.writeFileSync("log.txt", `${err.toString()} \n`, { flag: "a+" });
   }
 };
 
@@ -28,8 +43,6 @@ const handlePatch = (req, res) =>
   res.json({ status: 200, msg: "Hi from Patch controller" });
 
 const handleDefault = (req, res) => res.send("404 | page not found");
-
-
 
 module.exports = {
   handleGetAllUsers,
