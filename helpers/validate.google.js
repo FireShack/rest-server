b807f41270a449c75786df0b3884fb72651aeaf3
Franco <francoguardini10@gmail.com>
2022-04-25 22:07:43 -0300

    sign-in method finished

--- helpers/validate.google.js
+++ helpers/validate.google.js
@@ -1,15 +1,13 @@
 const { OAuth2Client } = require("google-auth-library");
-const client = new OAuth2Client(CLIENT_ID);
-async function verify() {
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+
+async function googleVerify(token = "") {
   const ticket = await client.verifyIdToken({
     idToken: token,
-    audience: CLIENT_ID, // Specify the CLIENT_ID of the app that accesses the backend
-    // Or, if multiple clients access the backend:
-    //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+    audience: process.env.GOOGLE_CLIENT_ID,
   });
-  const payload = ticket.getPayload();
-  const userid = payload["sub"];
-  // If request specified a G Suite domain:
-  // const domain = payload['hd'];
+  const { name, email, picture } = ticket.getPayload();
+  return { name, email, picture };
 }
-verify().catch(console.error);
+
+module.exports = { googleVerify };
