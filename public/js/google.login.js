706af0d54caf0b35f9e5ce2dd2eeec0d364ebfb5
Franco <francoguardini10@gmail.com>
2022-04-26 09:50:01 -0300

    google signOut finished

--- public/js/google.login.js
+++ public/js/google.login.js
@@ -1,9 +1,5 @@
+const logOutButton = document.querySelector("#google-logout");
 function handleCredentialResponse(response) {
-  // Google token
-  // console.log(response.credential);
-  //const url = "http://localhost:5000/api/auth/login/google";
-  //const res = await fetch(url);
-  //const data = res.json();
   const body = { id_token: response.credential };
   fetch("http://localhost:5000/api/auth/login/google", {
     method: "POST",
@@ -13,5 +9,18 @@ function handleCredentialResponse(response) {
     body: JSON.stringify(body),
   })
     .then((res) => res.json())
-    .then((res) => console.log(res));
+    .then((res) => {
+      console.log(res);
+      localStorage.setItem("email", res.googleUser.mail);
+    });
 }
+
+const googleLogOut = () => {
+  console.log("Logging out");
+  google.accounts.id.revoke(localStorage.getItem("email"), (done) => {
+    localStorage.clear();
+    location.reload();
+  });
+};
+
+logOutButton.addEventListener("click", googleLogOut);
